3195eefec35571a9dbc702808b06d2c8e9db4aab
Tomos Ewe <Tomos Ewe>
2016-12-13 22:31:53 +1300

    add grid

--- projects/js/sketch.js
+++ projects/js/sketch.js
@@ -1,7 +1,30 @@
-function setup() {
+var height;
+var width;
 
+function setup() {
+    width = 400;
+    height = 400;
+    var gameCanvas = createCanvas(width, height);
+    gameCanvas.parent('game-container');
+	fill(0);
 }
 
 function draw() {
-    
+	background(240);
+	drawGrid();
+	fill(0);
+	rect(60, 60, 100, 100);
+}
+
+function drawGrid() {
+	stroke(200);
+	fill(120);
+	for (var x=-width; x < width; x+=40) {
+		line(x, -height, x, height);
+		text(x, x+1, 12);
+	}
+	for (var y=-height; y < height; y+=40) {
+		line(-width, y, width, y);
+		text(y, 1, y+12);
+	}
 }
