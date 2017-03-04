1684a73b793fb3ca1c1858bcb7cf55319f09512e
TOMOS EWE <tomosewe@gmail.com>
2017-03-04 22:14:24 +1300

    handle screen wrapping

---
+++ games/spacebattle/js/Ship.js
@@ -0,0 +1,79 @@
+const GROUNDSPEED_DECAY_MULT = 0.94;
+const DRIVE_POWER = 0.5;
+const REVERSE_POWER = 0.2;
+const TURN_RATE = 0.03;
+const MIN_TURN_SPEED = 0.5;
+
+function shipClass() {
+  this.x = 75;
+  this.y = 75;
+
+  this.keyHeld_Gas = false;
+  this.keyHeld_Reverse = false;
+  this.keyHeld_TurnLeft = false;
+  this.keyHeld_TurnRight = false;
+
+  this.setupControls = function (forwardKey, backKey, leftKey, rightKey) {
+    this.controlKeyForGas = forwardKey;
+    this.controlKeyForReverse = backKey;
+    this.controlKeyForTurnLeft = leftKey;
+    this.controlKeyForTurnRight = rightKey;
+  }
+
+  this.init = function (whichGraphic) {
+    this.myBitmap = whichGraphic;
+    this.reset();
+  }
+
+  this.reset = function () {
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
+    this.speed = 0;
+    this.ang = -0.5 * Math.PI;
+  }
+
+  this.handleScreenWrap = function () {
+    if (this.x > canvas.width) {
+      this.x = 0;
+    }
+    if (this.x < 0) {
+      this.x = canvas.width;
+    }
+    if (this.y > canvas.height) {
+      this.y = 0;
+    }
+    if (this.y < 0) {
+      this.y = canvas.height;
+    }
+  }
+
+  this.move = function () {
+    if (Math.abs(this.speed) > MIN_TURN_SPEED) {
+      if (this.keyHeld_TurnLeft) {
+        this.ang -= TURN_RATE * Math.PI;
+      }
+
+      if (this.keyHeld_TurnRight) {
+        this.ang += TURN_RATE * Math.PI;
+      }
+    }
+
+    if (this.keyHeld_Gas) {
+      this.speed += DRIVE_POWER;
+    }
+    if (this.keyHeld_Reverse) {
+      this.speed -= REVERSE_POWER;
+    }
+
+    this.x += Math.cos(this.ang) * this.speed;
+    this.y += Math.sin(this.ang) * this.speed;
+
+    this.speed *= GROUNDSPEED_DECAY_MULT;
+    this.handleScreenWrap();
+  }
+
+  this.draw = function () {
+    drawBitmapCenteredAtLocationWithRotation(this.myBitmap, this.x, this.y, this.ang);
+  }
+
+} 
