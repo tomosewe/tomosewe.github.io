92790201963b472eb941c1ee34f2a77f6a1aa2ec
TOMOS EWE <tomosewe@gmail.com>
2017-03-07 22:59:06 +1300

    Group of units move to random spot in area

--- games/rts/js/Main.js
+++ games/rts/js/Main.js
@@ -1,10 +1,11 @@
-// save the canvas for dimensions, and its 2d context for drawing to it
+const PLAYER_START_UNITS = 8;
+
 var canvas, canvasContext;
-var testUnit = new unitClass();
+var playerUnits = [];
 
 window.onload = function () {
   canvas = document.getElementById('gameCanvas');
-  canvasContext = canvas.getContext('2d');  
+  canvasContext = canvas.getContext('2d');
 
   // these next few lines set up our game logic and render to happen 30 times per second
   var framesPerSecond = 30;
@@ -17,13 +18,18 @@ window.onload = function () {
     var mousePos = calculateMousePos(evt);
     document.getElementById("debugText").innerHTML = "(" + mousePos.x + "," + mousePos.y + ")";
   });
-  canvas.addEventListener('click', function(evt) {
+  canvas.addEventListener('click', function (evt) {
     var mousePos = calculateMousePos(evt);
-    testUnit.gotoX = mousePos.x;
-    testUnit.gotoY = mousePos.y;
+    for (var i = 0; i < playerUnits.length; i++) {
+      playerUnits[i].gotoNear(mousePos.x, mousePos.y);
+    }
   });
 
-  testUnit.reset();
+  for (var i = 0; i < PLAYER_START_UNITS; i++) {
+    var spawnUnit = new unitClass();
+    spawnUnit.reset();
+    playerUnits.push(spawnUnit);
+  }
 }
 
 function calculateMousePos(evt) {
@@ -39,11 +45,16 @@ function calculateMousePos(evt) {
 }
 
 function moveEverything() {
-  testUnit.move();
+  for (var i = 0; i < playerUnits.length; i++) {
+    playerUnits[i].move();
+  }
 }
 
 function drawEverything() {
   // clear the game view by filling it with black
   colorRect(0, 0, canvas.width, canvas.height, 'black');
-  testUnit.draw();
+
+  for (var i = 0; i < playerUnits.length; i++) {
+    playerUnits[i].draw();
+  }
 }
--- games/rts/js/Unit.js
+++ games/rts/js/Unit.js
@@ -1,5 +1,6 @@
 const UNIT_PLACEHOLDER_RADIUS = 5;
 const UNIT_PIXELS_MOVE_RATE = 2;
+const UNIT_MAX_RAND_DIST_FROM_WALK_TARGET = 50;
 
 function unitClass() {
 
@@ -17,19 +18,24 @@ function unitClass() {
     }
   }
 
-  this.move = function () {
-    if (this.x < this.gotoX) {
-      this.x += UNIT_PIXELS_MOVE_RATE;
-    }
-    if (this.x > this.gotoX) {
-      this.x -= UNIT_PIXELS_MOVE_RATE;
-    }
-    if (this.y < this.gotoY) {
-      this.y += UNIT_PIXELS_MOVE_RATE;
-    }
-    if (this.y > this.gotoY) {
-      this.y -= UNIT_PIXELS_MOVE_RATE;
-    }
+  this.gotoNear = function (aroundX, aroundY) {
+    this.gotoX = aroundX + Math.random() * UNIT_MAX_RAND_DIST_FROM_WALK_TARGET;
+    this.gotoY = aroundY + Math.random() * UNIT_MAX_RAND_DIST_FROM_WALK_TARGET;
   }
 
+  this.move = function () {
+    var deltaX = this.gotoX - this.x;
+    var deltaY = this.gotoY - this.y;
+    var distToGo = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    var moveX = UNIT_PIXELS_MOVE_RATE * deltaX / distToGo;
+    var moveY = UNIT_PIXELS_MOVE_RATE * deltaY / distToGo;
+
+    if (distToGo > UNIT_PIXELS_MOVE_RATE) {
+      this.x += moveX;
+      this.y += moveY;
+    } else {
+      this.x = this.gotoX;
+      this.y = this.gotoY;
+    }
+  }
 }
