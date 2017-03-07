6ca098d7c903f61c9d1e0e488ae5b652eebb113e
TOMOS EWE <tomosewe@gmail.com>
2017-03-07 22:05:17 +1300

    Click to move unit

--- games/rts/js/Main.js
+++ games/rts/js/Main.js
@@ -1,5 +1,30 @@
 // save the canvas for dimensions, and its 2d context for drawing to it
 var canvas, canvasContext;
+var testUnit = new unitClass();
+
+window.onload = function () {
+  canvas = document.getElementById('gameCanvas');
+  canvasContext = canvas.getContext('2d');  
+
+  // these next few lines set up our game logic and render to happen 30 times per second
+  var framesPerSecond = 30;
+  setInterval(function () {
+    moveEverything();
+    drawEverything();
+  }, 1000 / framesPerSecond);
+
+  canvas.addEventListener('mousemove', function (evt) {
+    var mousePos = calculateMousePos(evt);
+    document.getElementById("debugText").innerHTML = "(" + mousePos.x + "," + mousePos.y + ")";
+  });
+  canvas.addEventListener('click', function(evt) {
+    var mousePos = calculateMousePos(evt);
+    testUnit.gotoX = mousePos.x;
+    testUnit.gotoY = mousePos.y;
+  });
+
+  testUnit.reset();
+}
 
 function calculateMousePos(evt) {
   var rect = canvas.getBoundingClientRect(), root = document.documentElement;
@@ -13,28 +38,12 @@ function calculateMousePos(evt) {
   };
 }
 
-window.onload = function () {
-  canvas = document.getElementById('gameCanvas');
-  canvasContext = canvas.getContext('2d');
-
-  // these next few lines set up our game logic and render to happen 30 times per second
-  var framesPerSecond = 30;
-  setInterval(function () {
-    moveEverything();
-    drawEverything();
-  }, 1000 / framesPerSecond);
-
-  canvas.addEventListener('mousemove', function (evt) {
-    var mousePos = calculateMousePos(evt);
-    document.getElementById("debugText").innerHTML = "(" + mousePos.x + "," + mousePos.y + ")";
-  });
-}
-
 function moveEverything() {
-
+  testUnit.move();
 }
 
 function drawEverything() {
   // clear the game view by filling it with black
   colorRect(0, 0, canvas.width, canvas.height, 'black');
+  testUnit.draw();
 }
